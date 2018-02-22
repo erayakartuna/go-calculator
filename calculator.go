2d429e5169a27a7766e50f8a56f6e14f644d4bf2
eray <erayakartuna@gmail.com>
2018-02-22 20:29:41 +0300

    while added

--- calculator.go
+++ calculator.go
@@ -12,36 +12,38 @@ var total = 0
 
 func main() {
 
-	fmt.Printf("Write a number\n")
-	reader := bufio.NewReader(os.Stdin)
-	text,_ := reader.ReadString('\n')
-	text = strings.Replace(text,"\n","",-1)
-	number,err := strconv.Atoi(text)
-	if err != nil {
-		// handle error
-		fmt.Println(err)
-		os.Exit(2)
+	for{
+		fmt.Printf("Write a number\n")
+		reader := bufio.NewReader(os.Stdin)
+		text,_ := reader.ReadString('\n')
+		text = strings.Replace(text,"\n","",-1)
+		number,err := strconv.Atoi(text)
+		if err != nil {
+			// handle error
+			fmt.Println(err)
+			os.Exit(2)
+		}
+
+		fmt.Printf("what action do you want to do (+,-,/,*) \n")
+		operation, _ := reader.ReadString('\n')
+		operation = strings.Replace(operation,"\n","",-1)
+
+		proccess(operation,number)
+
+		fmt.Printf("Result = " + strconv.Itoa(total) + "\n")
+		fmt.Printf("do you want to continue ? Y/N \n")
+
+		operation, _ = reader.ReadString('\n')
+		operation = strings.Replace(operation,"\n","",-1)
+
+		//break loop
+		if operation == "N"{
+			break
+		}
+
+		fmt.Printf("Finished \n")
 	}
 
-	fmt.Printf("what action do you want to do (+,-,/,*) \n")
-	operation, _ := reader.ReadString('\n')
-	operation = strings.Replace(operation,"\n","",-1)
-
-	proccess(operation,number)
-
-	fmt.Printf("Result = " + strconv.Itoa(total) + "\n")
-	fmt.Printf("do you want to continue ? Y/N \n")
-
-	operation, _ = reader.ReadString('\n')
-	operation = strings.Replace(operation,"\n","",-1)
-
-	//loop
-	if operation == "Y"{
-		main()
-		return;
-	}
-
-	fmt.Printf("Finished \n")
 }
 
 
